40f8fb6ad1ecb06a3a54d0c2641d32529044c582
Dheemanth Regati <dheemanthregati@gmail.com>
2018-01-21 20:05:27 +0530

    commit

---
+++ pancake1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void flip(){
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i=0;i<n;i++){
+        ar[i]=(i+1);
+    }
+
+}
