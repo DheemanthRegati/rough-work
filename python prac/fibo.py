e63ece80463423d5151392ab085ee563ba70a9a2
Dheemanth Regati <dheemanthregati@gmail.com>
2018-05-25 17:31:25 +0530

    commit

--- python prac/fibo.py
+++ python prac/fibo.py
@@ -1,8 +1,8 @@
-c= input("enter number")
-a,b,d = 1,1,0
-print a
-while  d < (c-1) :
-    print b
-    a,b = b,a+b
-    d+=1
+C = input("enter number")
+A, B, D = 1, 1, 0
+print A
+while  D < (C-1):
+    print B
+    A, B = B, A+B
+    D += 1
 
